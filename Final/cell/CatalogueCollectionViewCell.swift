caa4d222c66c3b032b895681927ec24b5ae88ba1
Ibragim <ibragim.kuanyshbay@gmail.com>
2021-02-24 22:43:06 +0600

    catalogue added

--- Final/cell/CatalogueCollectionViewCell.swift
+++ Final/cell/CatalogueCollectionViewCell.swift
@@ -6,13 +6,19 @@ class CatalogueCollectionViewCell: UICollectionViewCell {
     static let identifier = String(describing: CatalogueCollectionViewCell.self)
     static let nib = UINib(nibName: identifier, bundle: nil)
     
-    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var imageLabel: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func commonInit(_ product: Product){
+        imageLabel.image = UIImage(named: product.image)
+        titleLabel.text = product.title
+        priceLabel.text = "\(product.price) $"
     }
 
 }
---
+++ Final/models/Product.swift
@@ -0,0 +1,15 @@
+//
+//  Product.swift
+//  Final
+//
+//  Created by Kuanyshbay Ibragim on 24.02.2021.
+//
+
+import Foundation
+
+
+struct Product {
+    var image: String
+    var title: String
+    var price: Double
+}
